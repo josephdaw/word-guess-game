8c7efe7575e3098b134472183bb65b2a057c91a6
josephdaw <josephdaw@icloud.com>
2021-09-23 23:22:42 +0930

    add comments and remove console.logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@ var timerEl = document.getElementById('countdown');
 var startEl = document.getElementById('start');
 var wordEl = document.getElementById('word');
 
+// function for when the 'start' button is clicked
 startEl.addEventListener("click", function() {
   countdown();
+  revealLetters();
 });
 
+// countdown timer function
 function countdown() {
   var timeLeft = 5;
 
@@ -30,20 +33,16 @@ document.addEventListener('keydown', function (event) {
   // Access value of pressed key with key property
   var key = event.key.toLowerCase();
 
-  console.log(key);
-
   for (let i = 0; i < secretWord.length; i++){
-    console.log(secretWord[i]);
-    
+    // check that the keypress matches a character in the secret word
     if (key == secretWord[i]) {
-      console.log("match")
       // add the key to revealedLetters[i]
       revealedLetters[i] = key;
       revealLetters();
+    }  // END - if 
+  } //END - for
+}); // END - function
 
-    }
-  }
-});
 
 function revealLetters() {
   wordEl.textContent = revealedLetters.join(" ");
